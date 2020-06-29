8ebc9007b2ef267f9b23a34dec8cae4d9f865e0e
Sam <wyysammore@gmail.com>
2020-06-29 17:41:33 +0800

    WIP: LinkedList，insert

--- DataStructuresAndAlgorithms/LinkedList/index.js
+++ DataStructuresAndAlgorithms/LinkedList/index.js
@@ -41,17 +41,49 @@ class LinkedList {
     this.count++;
   }
 
+  getElementAt(index) {
+    if (index >= 0 && index <= this.count) {
+      let node = this.head;
+      for (let i = 0; i < index && node != null; i++) {
+        node = node.next;
+      }
+      return node;
+    }
+    return undefined;
+  }
+
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new Node(element);
+      if (index === 0) {
+        const current = this.head;
+        node.next = current;
+        this.head = node;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        const current = previous.next;
+        node.next = current;
+        previous.next = node;
+      }
+      this.count++;
+      return true;
+    }
+    return false;
+  }
+
   removeAt(index) {
     if (index >= 0 && index < this.count) {
       let current = this.head;
       if (index === 0) {
         this.head = current.next;
       } else {
-        let previous;
+        /* let previous;
         for (let i = 0; i < index; i++) {
           previous = current;
           current = current.next;
-        }
+        } */
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
         previous.next = current.next;
       }
       this.count--;
@@ -64,7 +96,11 @@ class LinkedList {
 const list = new LinkedList();
 list.push(15);
 list.push(10);
-list.push(5);
+// list.push(5);
 console.log('list :>> ', list);
-console.log('list.removeAt(1) :>> ', list.removeAt(1));
-console.log('list2 :>> ', list);
+// console.log('list.removeAt(1) :>> ', list.removeAt(1));
+// console.log('list2 :>> ', list);
+list.insert(9, 1);
+console.log('list3 :>> ', list);
+list.insert(8, 2);
+console.log('list4 :>> ', list);
