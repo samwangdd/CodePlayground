8663c651d24974c2031b29f5834bed8f6a7f156e
sam <wyysammore@gmail.com>
2020-09-16 23:35:22 +0800

    feat(30s): pull.js

---
+++ 30S/pull.js
@@ -0,0 +1,16 @@
+/**
+ * 过滤指定元素，同时改变原数组
+ *
+ * @param {*} arr
+ * @param {*} args
+ */
+const pull = (arr, ...args) => {
+  let argState = Array.isArray(args[0]) ? args[0] : args;
+  let pulled = arr.filter((v) => !argState.includes(v));
+  arr.length = 0;
+  pulled.forEach((v) => arr.push(v));
+};
+
+let myArray = ['a', 'b', 'c', 'a', 'b', 'c'];
+pull(myArray, ['a', 'c']); // myArray = [ 'b', 'b' ]
+console.log('myArray :>> ', myArray);
