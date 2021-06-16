d09d78233bf04570129a263e354c43a448bc029d
samwangdd <wyysammore@gmail.com>
2021-06-16 15:56:44 +0800

    feat(ts): Permutation

---
+++ TypeScript/Medium/Permutation.ts
@@ -0,0 +1,13 @@
+// https://github.com/type-challenges/type-challenges/issues/614
+type perm = Permutation<'A' | 'B' | 'C'>; // ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']
+
+type IsNever<T> = [T] extends [never] ? true : false;
+
+// FIXME: 没看懂
+// union 范型怎么表示？
+type PermuteItem<Union, Item, Rest = Exclude<Union, Item>> =
+  IsNever<Rest> extends true ? [Item] : [Item, ...Permutation<Rest>];
+
+type Permutation<Union, Item = Union> = Item extends Item
+  ? PermuteItem<Union, Item>
+  : never;
