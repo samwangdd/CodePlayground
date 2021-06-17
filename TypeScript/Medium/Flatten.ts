6e476566115a2557d812f03108b25971e575b2b7
samwangdd <wyysammore@gmail.com>
2021-06-17 14:29:42 +0800

    feat(ts): Flatten

---
+++ TypeScript/Medium/Flatten.ts
@@ -0,0 +1,8 @@
+type flatten = Flatten<[1, 2, [3, 4], [[[5]]]]>;
+// [1, 2, 3, 4, 5]
+
+type Flatten<T extends unknown[]> = T extends [infer F, ...infer R]
+  ? F extends unknown[]
+    ? [...Flatten<F>, ...Flatten<R>]
+    : [F, ...Flatten<R>]
+  : T;
